31822c38edf951381e5fc89b203a059137166563
Saidafzal Kholkhujaev <saidafzal1209@gmail.com>
2024-07-19 23:43:38 +0500

    bug fix

--- src/shop_bot/shop_bot.module.ts
+++ src/shop_bot/shop_bot.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { ShopService } from './shop_bot.service';
 import { ShopUpdate } from './shop_bot.update';
 import { PrismaModule } from 'src/prisma/prisma.module';
@@ -6,7 +6,7 @@ import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [ShopService, ShopUpdate, UsersService],
+  providers: [ShopService, ShopUpdate, UsersService, Logger],
   controllers: [],
 })
 export class ShopModule {}
